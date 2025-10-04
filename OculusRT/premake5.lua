615974e024b2a0dabb2d4367b7a6d49100c74690
siddhantcvdi <dev.siddhantcvdi@gmail.com>
2025-10-04 16:39:45 +0530

    Basic Image Rendering

--- OculusRT/premake5.lua
+++ OculusRT/premake5.lua
@@ -1,4 +1,4 @@
-project "WalnutApp"
+project "OculusRT"
    kind "ConsoleApp"
    language "C++"
    cppdialect "C++17"
---
+++ OculusRT/src/WalnutApp.cpp
@@ -0,0 +1,83 @@
+#include "Walnut/Application.h"
+#include "Walnut/EntryPoint.h"
+
+#include "Walnut/Image.h" 
+#include "Walnut/Random.h"
+#include "Walnut/Timer.h"
+
+using namespace Walnut;
+
+class ExampleLayer : public Walnut::Layer
+{
+public:
+	virtual void OnUIRender() override
+	{
+		ImGui::Begin("Settings");
+		ImGui::Text("Last Render %.3fms", m_LastRenderTime);
+		if (ImGui::Button("Render")) {
+			Render();
+		}
+		ImGui::End();
+
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("ViewPort");
+		// Get Viewport Width and Height from built in ImGui Functions
+		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
+		// Render Image if Available
+		if (m_Image)
+			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+		ImGui::End();
+		ImGui::PopStyleVar();
+
+	}
+
+	void Render() {
+
+		Timer timer;
+		// If there is no image or viewport height and width have changed then only create a new Image and new Image Buffer acc to the new viewport, also delete the old image buffer
+		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight()) {
+			m_Image = std::make_shared<Walnut::Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
+			delete[] m_ImageData;
+			m_ImageData = new uint32_t[m_ViewportHeight * m_ViewportWidth];
+		}
+
+		// Set Random pixel value ABGR - (8 bits each for RGBA - uint32)
+		for (uint32_t i = 0; i < m_ViewportHeight * m_ViewportWidth; i++) {
+			m_ImageData[i] = Random::UInt();
+			// To make the A value 100%
+			m_ImageData[i] |= 0xff000000;
+		}
+
+		// Link image data to m_Image
+		m_Image->SetData(m_ImageData);
+
+		m_LastRenderTime = timer.ElapsedMillis();
+	}
+private:
+	std::shared_ptr<Walnut::Image> m_Image;
+	uint32_t* m_ImageData = nullptr;
+	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
+	float m_LastRenderTime = 0.0f;
+};
+
+Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
+{
+	Walnut::ApplicationSpecification spec;
+	spec.Name = "Sidd Ray Tracer";
+
+	Walnut::Application* app = new Walnut::Application(spec);
+	app->PushLayer<ExampleLayer>();
+	app->SetMenubarCallback([app]()
+		{
+			if (ImGui::BeginMenu("File"))
+			{
+				if (ImGui::MenuItem("Exit"))
+				{
+					app->Close();
+				}
+				ImGui::EndMenu();
+			}
+		});
+	return app;
+}
