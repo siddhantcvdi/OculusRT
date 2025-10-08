b1a8ff6cc00e3b5e1a96c90dfe10d357f5645940
siddhantcvdi <dev.siddhantcvdi@gmail.com>
2025-10-08 14:50:27 +0530

    Hitpoint based coloring

--- OculusRT/src/Renderer.cpp
+++ OculusRT/src/Renderer.cpp
@@ -1,6 +1,17 @@
 #include "Renderer.h"
 #include "Walnut/Random.h"
 
+namespace Utils{
+	static uint32_t ConvertToRGBA(const glm::vec4& color) {
+		uint8_t r = (uint8_t)(color.r * 255.0f);
+		uint8_t g = (uint8_t)(color.g * 255.0f);
+		uint8_t b = (uint8_t)(color.b * 255.0f);
+		uint8_t a = (uint8_t)(color.a * 255.0f);
+		uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
+		return result;
+	}
+}
+
 void Renderer::Render() {
 	// render every pixel
 	// Set Random pixel value ABGR - (8 bits each for RGBA - uint32)
@@ -14,7 +25,9 @@ void Renderer::Render() {
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++) {
 			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight()};
 			coord = coord * 2.0f - 1.0f; // -1 -> 1
-			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
+			glm::vec4 color = PerPixel(coord);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
 		}
 	}
 
@@ -37,13 +50,13 @@ void Renderer::OnResize(uint32_t width, uint32_t height) {
 	m_ImageData = new uint32_t[width * height];
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord) {
+glm::vec4 Renderer::PerPixel(glm::vec2 coord) {
 
 	uint8_t r = (uint8_t)(coord.x * 255.0f);
 	uint8_t g = (uint8_t)(coord.y * 255.0f);
 
 	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
-	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
+	glm::vec3 rayOrigin(0.0f, 0.0f, 1.0f);
 	float radius = 0.5f;
 	// (bx^2 + by^2 + bz^2)t^2 + (2(axbx + ayby))t + (ax^2 + ay^2 - r^2) = 0 
 
@@ -59,6 +72,17 @@ uint32_t Renderer::PerPixel(glm::vec2 coord) {
 
 	float discriminant = b * b - 4.0f * a * c;
 	
-	if (discriminant >= 0) return 0xff0000ff;
-	return 0xff000000;
+	if (discriminant < 0) return glm::vec4(0, 0, 0, 1);
+
+	float t0 = (-b + glm::sqrt(discriminant)) / 2.0f * a;
+	float closestT = (-b - glm::sqrt(discriminant)) / 2.0f * a;
+
+	glm::vec3 closestHitPoint = rayOrigin + rayDirection * closestT;
+
+	//glm::vec3 h1 = rayOrigin + rayDirection * t1;
+	glm::vec3 sphereColor = closestHitPoint;
+
+	return glm::vec4(sphereColor, 1.0f);
+
+
 }
